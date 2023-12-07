0eac12499b5ac96ab685a8cc58179b52ce4d4ea7
evita_local <evita_96@windowslive.com>
2023-12-07 23:54:41 +0100

    evita_local at Thu Dec  7 23:54:41 CET 2023

--- auto_git.sh
+++ auto_git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 # Navigate to directory.
 cd "$1"
@@ -13,7 +14,7 @@ do
 #  echo "No changes to commit"
 #  else
  git add .
- git commit -m "$(git config user.name) at $(date)" && yes | git push && echo -e "${RED}Changes pushed at $(date)" 
+ git commit -m "$(git config user.name) at $(date)" && yes | git push && echo -e "${RED}Changes pushed at $(date).${NC}" 
 #  fi
  sleep 30
 done
