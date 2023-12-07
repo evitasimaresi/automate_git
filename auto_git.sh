88b6fb1ae9d59ae1e02005d3e005bd92af9c380b
evita_local <evita_96@windowslive.com>
2023-12-07 23:37:14 +0100

    evita_local at Thu Dec  7 23:37:14 CET 2023

--- auto_git.sh
+++ auto_git.sh
@@ -5,14 +5,13 @@ cd "$1"
 
 while true
 do
- if git diff --quiet
- then
- echo "No changes to commit"
- else
+#  if git diff --quiet
+#  then
+#  echo "No changes to commit"
+#  else
  git add .
- git commit -m "$(git config user.name) at $(date)"
- yes | git push
- fi
- echo -ne '\n' | read
- sleep 180
+ git commit -m "$(git config user.name) at $(date)" && yes | git push
+#  fi
+#  echo -ne '\n' | read
+ sleep 30
 done
